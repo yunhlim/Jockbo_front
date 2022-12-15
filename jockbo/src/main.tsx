d7d918adccb55cccb6554980fba09cfbad0bc8eb
yunh <yunh04131@gmail.com>
2022-12-15 23:59:21 +0900

    feat: 기본 폴더 구조 잡기 vite 사용

---
+++ jockbo/src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);

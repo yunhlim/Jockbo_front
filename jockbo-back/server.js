ce5b772c418d91ffe573949fe77f122a9a4a6e2c
yunh <yunh04131@gmail.com>
2022-12-30 12:57:26 +0900

    feat: front와 back 단 합치기 back 코드를 front 레포로 옮기고 폴더 명과 레포 명 수정

---
+++ jockbo-back/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+// app.use(express.urlencoded({ extended: true })); //slu : nodejs 에서 html body를 제대로 읽어오려면 이 옵션을 넣어줘야함
+app.use(express.json());
+app.use(cors());
+var db;
+MongoClient.connect(
+  "mongodb+srv://jokbo:1111@cluster0.kawqw2r.mongodb.net/?retryWrites=true&w=majority",
+  function (e, client) {
+    if (e) return console.log(e);
+    app.listen(8080, function () {
+      console.log("listening on 8080");
+      db = client.db("jokbonode");
+    });
+  }
+);
+
+app.set("view engine", "ejs");
+
+app.get("", function (r, a) {
+  a.send("server on duty");
+});
+app.get("/admin", function (r, a) {
+  a.sendFile(__dirname + "/admin.html");
+});
+app.post("/create", function (r, a) {
+  a.send("create 실행됨");
+  console.log("UID " + r.body._id + " 에 대한 정보 생성 시작");
+  db.collection("post").insertOne(
+    {
+      _id: r.body._id,
+      ancUID: r.body.ancUID,
+      mySae: r.body.mySae,
+      myName: r.body.myName,
+      myNamechi: r.body.myNamechi,
+      ect: r.body.ect,
+      moddate: r.body.moddate,
+    },
+    function (e, r) {
+      console.log("에러사항 : " + e);
+      console.log("결과 : ");
+      console.log(r);
+      console.log("저장완료");
+    }
+  );
+});
+app.get("/list", function (r, a) {
+  db.collection("post")
+    .find()
+    .toArray(function (e, r) {
+      console.log(r);
+      a.send(r);
+      // a.render("list.ejs", { result: r });
+    });
+});

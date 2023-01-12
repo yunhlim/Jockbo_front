2e022b562973fafcfe10de2054db6bdf531f20ed
yunh <yunh04131@gmail.com>
2023-01-12 16:35:42 +0900

    feat: 최상단 사이트 이름 변경

--- jockbo-front/src/routes/Root.tsx
+++ jockbo-front/src/routes/Root.tsx
@@ -4,7 +4,8 @@ import NavBar from './NavBar';
 export default function Root() {
   return (
     <div>
-      <h1 style={{ display: 'inline' }}>안동장씨 온라인 족보</h1>
+      {/* <h1 style={{ display: 'inline' }}>安東張氏 利川伯派 南海宗親會</h1> */}
+      <h1 style={{ display: 'inline' }}>안동장씨 이천백파 남해종친회</h1>
       <nav>
         <NavBar />
       </nav>
